1c690c8ad0c2f1dd9a92e94cf0262626aa21fb14
Vixthor <tega2112@gmail.com>
2022-10-14 16:29:20 -0700

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/* 
- * main - prints a line of code using printf
+/*
+ * main - Prints out a code using the printf function
  *
  * Return: 0
-*/
+ */
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
