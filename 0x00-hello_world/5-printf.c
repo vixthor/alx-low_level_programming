849d72c32bc8e5b0d4f2498e1842fc984fd612b7
Vixthor <tega2112@gmail.com>
2022-10-14 15:58:19 -0700

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /*
  * printf - this is a function to print a value 
+ * main - holds my main code
  * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
-
