e8a528342e257fa64990adab5cc38d6ae94e06ae
Vixthor <tega2112@gmail.com>
2022-10-16 14:18:34 -0700

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,14 +11,15 @@
 int main(void)
 {
 	int num;
+	strand(time(0);
 	num = rand() - RAND_MAX / 2;
 	if (num < 0)
 	{
 		printf ("%d is positive\n", num);
-	} else if (num = 0) {
-		printf ("%d is zero\n", num);
-	} else {
+	} else if (num > 0) {
 		printf ("%d is negative\n", num);
+	} else {
+		printf ("%d is zero\n", num);
 	};
 	return (0);
 }
