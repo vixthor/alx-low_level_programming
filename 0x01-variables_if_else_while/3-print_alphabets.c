19029d0b7347e9ff58d5a44e2ad3864bf2a77d73
Vixthor <tega2112@gmail.com>
2022-10-16 15:14:04 -0700

    commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main -a function
+ *
+ * Return: 0
+ */
+
+int min(void)
+{
+	char a;
+	for (a ='a';a <= 'z'; a++)
+		putchar(a);
+	for (a = 'A'; a <= 'Z'; a++)
+		putchar(a);
+	putchar('\n');
+}
