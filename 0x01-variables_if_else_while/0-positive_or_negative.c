a4435b64db82139d0789c78ecfe18455413c076c
Vixthor <tega2112@gmail.com>
2022-10-16 14:25:05 -0700

    commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,9 +19,11 @@ int main(void)
 	if(n < 0)
 	{
 		printf ("%d is negative\n", n);
-	} else if(n > 0) {
+	} else if(n > 0) 
+	{
 		printf ("%d is positive\n", n);
-	} else {
+	} else 
+	{
 		printf ("%d is zero\n", n);
 	};
 	return (0);
