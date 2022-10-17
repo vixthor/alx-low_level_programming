3fb4ed6f3ef52f5a834ac7733d01be48c4442c5c
Vixthor <tega2112@gmail.com>
2022-10-17 04:25:22 -0700

    c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,8 +11,13 @@ int main(void)
 	for (a = 0 ; a < 10; a++)
 	{
 		putchar((a % 10) + '0');
+		if (a == 9)
+			continue;
 		putchar(',');
 		putchar(' ');
 	};
 	putchar("\n");
+
+	return (0);
+}
 
