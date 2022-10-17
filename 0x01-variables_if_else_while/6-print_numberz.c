384daa31b359a7dbe6b15c1e82d084970173f53d
Vixthor <tega2112@gmail.com>
2022-10-17 03:44:13 -0700

    c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - this is a function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 0 ; a < 10 ; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
