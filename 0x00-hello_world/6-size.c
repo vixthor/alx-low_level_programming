3a8f29ab5341ce7419a3fc24112312a8c9db5f4d
Vixthor <tega2112@gmail.com>
2022-10-17 05:19:59 -0700

    c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(D));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(A));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(B));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sof(C));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(C));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(F));
 	return (0);
 }
