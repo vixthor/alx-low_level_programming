0d8d9a937c0129e018763ecdfa8bef7034103d5c
Vixthor <tega2112@gmail.com>
2022-10-19 16:05:12 -0700

    c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints the aplhabet in lowercase
+ *
+ * Return: 0
+ */
+int main (void)
+{
+	print_alphabet()
+	{
+		char a;
+		for (a = 'a' ; a <= 'z' ; a++)
+			putchar(a);
+		putchar('\n');
+	}
+	return (0);
+		
+}
