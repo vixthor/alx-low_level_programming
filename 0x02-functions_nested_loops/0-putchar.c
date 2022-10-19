7cfa0c86225df4f1995f67c9ca82271fc920c596
Vixthor <tega2112@gmail.com>
2022-10-19 15:25:12 -0700

    c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print out putchar and new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	putchar("\_");
+	putchar("p");
+	putchar("u");
+	putchar("t");
+	putchar("c");
+	putchar("h");
+	putchar("a");
+	putchar("r");
+	putchar("\n");
+}
