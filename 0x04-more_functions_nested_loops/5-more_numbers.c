e4ec7b20d9d0e4e9a11cc3c0f2a903afd671848d
Vixthor <tega2112@gmail.com>
2022-10-20 16:32:40 -0700

    c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - prints numbers 10 times
+ * Retrun: value
+ */
+void more_numbers(void)
+{
+	char a;
+	int n;
+
+	n = 0;
+	while (n < 11)
+	{
+		for (a = '0' ; a <= 14 ; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+		n++;
+	}
+}
+
