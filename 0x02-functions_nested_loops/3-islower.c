e08a22c7cda55ba8dacfc9d9bf127fc5001e92eb
Vixthor <tega2112@gmail.com>
2022-10-20 01:56:25 -0700

    c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,11 +3,13 @@
  * _islower - checks if character is lowercase
  *
  * Return: 0 or 1
+ *
+ * c - contains a value
  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-	       return (1);
+		return (1);
 	else
 		return (0);
 }
