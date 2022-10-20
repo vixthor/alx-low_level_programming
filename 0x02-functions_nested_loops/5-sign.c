844fd8b8e20ebe4b8bcf997fd306f2a3ea7dc1f1
Vixthor <tega2112@gmail.com>
2022-10-20 07:10:02 -0700

    c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,7 @@
 #include "main.h"
 /**
  * print_sign - print out the sign of the number inputed
- *
  * @n: the value to be inputed
- *
  * Returns 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
