274c32b23de9d15fee130eaac2df48c7ee18afe9
Vixthor <tega2112@gmail.com>
2022-10-20 07:51:34 -0700

    c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - finds the absolute value of  number
+ * @n: the value
+ * Return: 0
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
