32853613755bf96cc680d89c0edef7b4cb9dc64b
Vixthor <tega2112@gmail.com>
2022-10-20 16:43:54 -0700

    c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,11 +6,9 @@
 void more_numbers(void)
 {
 	char a;
-
 	int n;
 
 	n = 0;
-
 	while (n < 11)
 	{
 		for (a = '0' ; a <= 14 ; a++)
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_line - prints a line n times
+ * @n: the value
+ * Return: value
+ */
+void print_line(int n)
+{
+	char a;
+	for (a = '0'; a <= n; a++)
+	{
+		if (n < 0)
+		{
+			break;
+		}
+		_putchar('_');
+	}
+	_putchar('\n');
+}
