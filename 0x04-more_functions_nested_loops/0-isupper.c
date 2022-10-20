f3979cce29969ae6989dcbf04b19b22677895647
Vixthor <tega2112@gmail.com>
2022-10-20 12:08:17 -0700

    c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * isupper - check if value is in uppercase
+ * @c: the value
+ * Return: 1 if c is uppercase
+ * 0 if not
+ */
+int _isupper(int c)
+{
+	if(c >= 'A' && c >= 'Z')
+		return (1);
+	else
+		return (0);
+}
