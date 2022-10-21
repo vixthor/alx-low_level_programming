b2c2f14ff159397ef4abb3ac5db482e9d59c5f64
Vixthor <tega2112@gmail.com>
2022-10-20 18:10:41 -0700

    c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,15 +7,13 @@ void more_numbers(void)
 {
 	int n, a;
 
-	
-
-	for (n = 0 ; n <= 10 ; n++)
+	for (n = 1 ; n <= 10 ; n++)
 	{
 		for (a = '0' ; a <= 14 ; a++)
 		{
 			if (a >= 10)
 				_putchar('1');
-			_putchar (j % 10 + '0');
+			_putchar (a % 10 + '0');
 		}
 		_putchar('\n');
 	}
