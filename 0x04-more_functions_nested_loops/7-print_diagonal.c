5ef05dd87689cfdcf118c62e7f4548202552fb65
Vixthor <tega2112@gmail.com>
2022-10-21 12:36:53 -0700

    c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_diagonal - prints diagonal
+ * @n: the value
+ * Reutrn: the value
+ */
+void print_diagonal(int n)
+{
+	int c;
+
+	for (c = '0'; c < n ; c++)
+		_putchar('\\');
+	_putchar('\n');
+}
