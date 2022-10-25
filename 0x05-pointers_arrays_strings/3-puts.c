488d798ba73397abc9f466b83dc685eab8c5e2f6
Vixthor <tega2112@gmail.com>
2022-10-25 06:56:24 -0700

    c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts: prints out a string
+ * @str: the string
+ * Return: the string
+ */
+
+void _puts(char *str)
+{
+	for(; *str++;)
+	{
+		return (s);
+	}
+	_putchar('\n');
+}
