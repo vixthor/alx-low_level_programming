3bc3e2f5d4f59fa85f7c7672929a98051aedac1c
Vixthor <tega2112@gmail.com>
2022-10-25 03:53:51 -0700

    c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - swaps values between two integers
+ * @a: the value
+ * @b: the second value
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = s;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,5 +3,6 @@
 
 void reset_to_98(int *n);
 char _putchar(char c);
+void swap_int(int *a, int *b);
 
 #endif
