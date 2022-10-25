3b2c575b17e7e3832c754fc300fd10caa1e54b74
Vixthor <tega2112@gmail.com>
2022-10-25 06:34:52 -0700

    c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * strlen - counts the length of a string
+ * @s: character
+ * Return: value
+ */
+int _strlen(char *s)
+{
+	int number;
+	
+	number = 0;
+	for (; *s++ ;)
+	{
+		number++;
+	}
+	return (number);
+}
