59089fe9b35a26f8a5fbfc37cf4c14ee114167cd
Vixthor <tega2112@gmail.com>
2022-10-29 07:49:18 -0700

    c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * strcmp - compare 2 strings
+ * @s1:string
+ * @s2:strmp
+ * Return:the number
+ *
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (*s1 - *s2);
+	}
+}
