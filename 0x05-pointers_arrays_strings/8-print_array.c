c76c7ca45bb22d4bf22ef3aae846a4fd0bcd2245
Vixthor <tega2112@gmail.com>
2022-10-29 02:58:30 -0700

    c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_array - prints out an array of number n
+ * @a: the first int
+ * @n: the second int
+ * Return: o
+ */
+
+void print_array(int *a, int n)
+{
+	int a;
+
+	for (x = 0; x < n; x++)
+		if (x != n - 1)
+			printf("%d", a[x]);
+		else
+			printf("%d", a[x]);
+	printf("\n");
+}
