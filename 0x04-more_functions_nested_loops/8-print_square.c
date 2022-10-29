94b9b72f4384592f5c1109583eb06f409ffd84bb
Vixthor <tega2112@gmail.com>
2022-10-29 06:25:32 -0700

    c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -15,14 +15,18 @@ void print_square(int size)
 	while (b < size)
 	{
 		if (size <= 0)
+		{
 			_putchar('\n');
 			break;
+		}
 		else
+		{
 			for (a = 0; a <= size; a++)
 			{
 				_putchar('#');
 			}
 			_putchar('\n');
+		}
 		b++;
 	}
 }
