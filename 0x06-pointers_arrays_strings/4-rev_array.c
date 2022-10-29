6eee4b8fce6f05b754d8012b4bd7141d05fd527d
Vixthor <tega2112@gmail.com>
2022-10-29 09:47:35 -0700

    c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- *  * reverse_array - reverse array
- *   * @a:array
- *    * @n:integer
- *     * Return:void
- *      */
+ * reverse_array - reverse array
+ * @a:array
+ * @n:integer
+ * Return:void
+ */
 void reverse_array(int *a, int n)
 {
 	int i, c;
