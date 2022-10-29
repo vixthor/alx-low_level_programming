fa945471a6c079a65cc0ec31fe5974675b482635
Vixthor <tega2112@gmail.com>
2022-10-28 17:55:09 -0700

    c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - prints half a string
+ * @str: the character
+ * Return: 0 if successful
+ */
+
+void puts_half(char *str)
+{
+	int string =0, n;
+
+	while (str[string] != '\0')
+	{
+		string++;
+	}
+	if (string + 1 % 2 != '0')
+		n = (string - 1) / 2;
+	else
+		n = (string / 2);
+	n++;
+
+	for (string = n; str[string] != '\0'; string ++)
+		_putchar(str[string]);
+	_putchar('\n');
+}
