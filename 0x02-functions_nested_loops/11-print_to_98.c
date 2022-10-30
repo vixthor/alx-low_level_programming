7223c6e578a8ee832d7147ffe95e81ea440d1f4d
Vixthor <tega2112@gmail.com>
2022-10-30 00:29:05 -0700

    c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#Include "main.h"
+/**
+ * print_to_98 - prints from n to 98
+ * @n: the integer
+ * Return: the result
+ */
+
+void print_to_98(int n)
+{
+	for(;n <= 98;n++)
+	{
+		printf("%d, ",n);
+	}
+}
