1eab9d26b7f01dab4cfffb9f8f56fcc549c0fe3e
Vixthor <tega2112@gmail.com>
2022-10-30 03:26:21 -0700

    c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,6 +7,15 @@
 
 void print_number(int n)
 {
-	unsigned int number = n
-	_putchar(number);
+	unsigned int number = n;
+
+	if (n < 0)
+	{
+		number = -number;
+	}
+	if (number / 10)
+	{
+		print_number(number / 10);
+	}
+	_putchar(number % 10 + '0');
 }
