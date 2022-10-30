7ba311d1b3d2485396660a39c9a2794d3ec91e17
Vixthor <tega2112@gmail.com>
2022-10-30 03:56:58 -0700

    c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,13 +9,10 @@ int main(void)
 	int sum, n;
 
 	sum = 0;
-	for (n =0; n < 1024; n++)
+	for (n = 0; n < 1024; n++)
 	{
-		if ((5 % n) == 0 || (n % 3) == 0)
-		{
+		if ((n % 5) == 0 || (n % 3) == 0)
 			sum += n;
-		}
-		
 	}
 	printf("%d\n", sum);
 	return (0);
