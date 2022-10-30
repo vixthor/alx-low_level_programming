814eacbcc4c069edf73e43da09957db1a072625f
Vixthor <tega2112@gmail.com>
2022-10-29 23:58:45 -0700

    c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,17 +11,17 @@ int main(void)
 
 	for (a = 0; a <= 100; a++)
 	{
-		if (a % 3 == 0)
+		if (a % 15 == 0)
 		{
-			printf("Fizz");
+			printf("FizzBuzz");
 		}
 		else if (a % 5 ==  0)
 		{
 			printf("Buzz");
 		}
-		else if (a % 15 == 0)
+		else if (a % 3 == 0)
 		{
-			printf("FizzBuzz");
+			printf("Fizz");
 		}
 		else
 		{
