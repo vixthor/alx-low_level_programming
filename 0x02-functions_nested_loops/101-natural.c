6ee7a0efe329d46666778ccb806042fac0cbd991
Vixthor <tega2112@gmail.com>
2022-10-30 03:40:05 -0700

    c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - prints out the sum of the multiples of 3 or 5 from 1024
+ * Return: sum
+ */
+
+int main(void)
+{
+	int sum, no;
+
+	n = 0
+	sum = 0;
+	while (n < 1025)
+	{
+		if (5 % n == 0 || n % 3 == 0)
+		{
+			sum += n;
+			n++;
+		}
+		else
+		{
+			n++;
+		}
+	}
+	printf("%d", sum);
+	return (0);
+}
