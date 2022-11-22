19fbb1c2917b670d1151346f47d5348872d4177b
Vixthor <tega2112@gmail.com>
2022-11-22 13:48:29 -0800

    c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,7 +10,7 @@ int check(int a, int b)
 {
 	if (a * a == b)
 		return (a);
-	if ( a * a > b)
+	if (a * a > b)
 		return (-1);
 	return (check(a + 1, b));
 }
@@ -24,5 +24,5 @@ int _sqrt_recursion(int n)
 {
 	if (n == 0)
 		return (0);
-	return(check(1,n));
+	return (check(1, n));
 }
