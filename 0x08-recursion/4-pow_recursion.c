6e77e6ae10c198da543b5ebec2c344efbc032ccb
Vixthor <tega2112@gmail.com>
2022-11-22 13:49:56 -0800

    c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,7 +2,7 @@
 /**
  * _pow_recursion - power to x
  * @x: int
- * @y; int
+ * @y: int
  * Return: int
  */
 
@@ -12,12 +12,12 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	else if ( y != 0)
+	else if (y != 0)
 	{
-		return (x * _pow_recursion(x, y -1));
+		return (x * _pow_recursion(x, y - 1));
 	}
 	else
 	{
-		return(1);
+		return (1);
 	}
 }
